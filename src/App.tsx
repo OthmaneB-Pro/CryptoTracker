bc67073d5732cc6f5c5e5bbdddc5428225fdb173
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-03-23 14:59:28 +0100

    ✨ Create ErrorPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import CryptoTracker from "./components/pages/crypto/CryptoTracker";
+import ErrorPage from "./components/pages/error/ErrorPage";
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path="*" element={<ErrorPage />} />
           <Route path="/" element={<CryptoTracker />} />
         </Routes>
       </BrowserRouter>
