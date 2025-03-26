c713edcab7287ddd5dda75e46c7742e5969768b0
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-03-26 15:39:21 +0100

    ✨ CryptoDetails : call api and return crypto details

--- src/components/pages/cryptoDetails/CryptoDetail.tsx
+++ src/components/pages/cryptoDetails/CryptoDetail.tsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { fetchCryptoDetails } from "../../../api/CoinGeckoApi";
+
+type CryptoType = {
+  id: string;
+  name: string;
+  symbol: string;
+  image: { large: string };
+  market_data: {
+    current_price: { usd: number };
+    price_change_percentage_24h: number;
+  };
+};
+
 export default function CryptoDetail() {
+  const { id } = useParams();
+  const [cryptoDetails, setCryptoDetails] = useState<CryptoType | null>(null);
+
+  useEffect(() => {
+    if (id) {
+      fetchCryptoDetails(setCryptoDetails, id);
+    }
+  }, [id]);
+
+  if (!cryptoDetails) return <p>Chargement...</p>;
+
   return (
-    <div>CryptoDetail</div>
-  )
+    <div>
+      <h1>{cryptoDetails.name}</h1>
+      <img src={cryptoDetails.image.large} alt={cryptoDetails.name} />
+      <p>{cryptoDetails.market_data.current_price.usd} $</p>
+      <p>{cryptoDetails.market_data.price_change_percentage_24h} %</p>
+    </div>
+  );
 }
