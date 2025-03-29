3857c69e878fa6aeed5afec93dd5b1b36483057e
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-03-29 17:25:58 +0100

    🚧 CryptoDetails : display charts

--- src/components/pages/cryptoDetails/CryptoDetail.tsx
+++ src/components/pages/cryptoDetails/CryptoDetail.tsx
@@ -1,8 +1,19 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { fetchCryptoDetails } from "../../../api/CoinGeckoApi";
+import {
+  fetchCryptoChart,
+  fetchCryptoDetails,
+} from "../../../api/CoinGeckoApi";
 import styled from "styled-components";
 import NavbarCrypto from "../crypto/NavbarCrypto";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 type CryptoType = {
   id: string;
@@ -14,16 +25,29 @@ type CryptoType = {
     price_change_percentage_24h: number;
   };
 };
+interface CryptoDetailsProps {
+  priceChange: number;
+}
 
 export default function CryptoDetail() {
   const { id } = useParams();
   const [cryptoDetails, setCryptoDetails] = useState<CryptoType | null>(null);
+  const [chartData, setChartData] = useState<number[][]>([]);
 
   useEffect(() => {
     if (id) {
       fetchCryptoDetails(setCryptoDetails, id);
+      fetchCryptoChart(setChartData, id);
     }
   }, [id]);
+
+  const formattedChartData = chartData.map(([timestamp, price]) => ({
+    time: new Date(timestamp).toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    }),
+    price,
+  }));
 
   if (!cryptoDetails) return <p>Chargement...</p>;
 
@@ -32,16 +56,26 @@ export default function CryptoDetail() {
       priceChange={cryptoDetails.market_data.price_change_percentage_24h}
     >
       <NavbarCrypto handleScroll={() => {}} />
+      <ResponsiveContainer width="90%" height={300}>
+        <LineChart data={formattedChartData}>
+          <XAxis dataKey="time" />
+          <YAxis domain={["auto", "auto"]} />
+          <Tooltip />
+          <Line
+            type="monotone"
+            dataKey="price"
+            stroke="#82ca9d"
+            strokeWidth={2}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+
       <h1>{cryptoDetails.name}</h1>
       <img src={cryptoDetails.image.large} alt={cryptoDetails.name} />
       <p>{cryptoDetails.market_data.current_price.usd} $</p>
       <p>{cryptoDetails?.market_data.price_change_percentage_24h} %</p>
     </CryptoDetailsStyled>
   );
-}
-
-interface CryptoDetailsProps {
-  priceChange: number;
 }
 
 const CryptoDetailsStyled = styled.div<CryptoDetailsProps>`
