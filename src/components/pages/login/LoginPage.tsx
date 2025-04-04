2d96dcd41decd1080da191e14e48be34f4c54a16
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-04-04 21:27:26 +0200

    ✨ Login : first rendered

--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+import NavbarCrypto from "../crypto/NavbarCrypto";
+
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <LoginStyled>
+      <NavbarCrypto handleScroll={() => {}} />
+      LoginPage</LoginStyled>
   )
 }
+const LoginStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: black;
+  color: white;
+  min-height: 100vh;
+`;
